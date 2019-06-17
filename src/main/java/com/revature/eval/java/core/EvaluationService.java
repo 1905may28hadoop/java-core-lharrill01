aad50da0a3a163b6f325d54626f968df88f1d221
lharrill01 <lharrill01@gmail.com>
2019-06-17 17:27:02 -0400

    submission

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -1,7 +1,14 @@
 package com.revature.eval.java.core;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 public class EvaluationService {
 
@@ -15,9 +22,17 @@ public class EvaluationService {
 	 */
 	public String acronym(String phrase) {
 		// TODO Write an implementation for this method declaration
-		return null;
+		String[] splitphrase = phrase.split(" |-");
+		String acryn = "";
+		for(int i = 0; i < splitphrase.length;i++) {
+			
+				acryn += splitphrase[i].charAt(0);
+				
+			}
+		 
+		return acryn.toUpperCase();
 	}
-
+	
 	/**
 	 * 2. Given a word, compute the scrabble score for that word.
 	 * 
@@ -35,7 +50,54 @@ public class EvaluationService {
 	 */
 	public int getScrabbleScore(String string) {
 		// TODO Write an implementation for this method declaration
-		return 0;
+		//Create a key for the program to follow
+
+	   
+//		Implement Hash Map
+		
+		string = string.toUpperCase();
+		String[] testStringA = string.toUpperCase().split("");
+
+	   Map<String, Integer> letval = new HashMap<String, Integer>();
+	   int sum = 0;
+	   
+	   
+	   letval.put("A", 1);
+	   letval.put("E", 1);
+	   letval.put("I", 1);
+	   letval.put("O", 1);
+	   letval.put("U", 1);
+	   letval.put("L", 1);
+	   letval.put("N", 1);
+	   letval.put("R", 1);
+	   letval.put("S", 1);
+	   letval.put("T", 1);
+	   letval.put("D", 2);
+	   letval.put("G", 2);
+	   letval.put("B", 3);
+	   letval.put("C", 3);
+	   letval.put("M", 3);
+	   letval.put("P", 3);
+	   letval.put("F", 4);
+	   letval.put("H", 4);
+	   letval.put("V", 4);
+	   letval.put("W", 4);
+	   letval.put("Y", 4);
+	   letval.put("K", 5);
+	   letval.put("X", 8);
+	   letval.put("Q", 10);
+	   letval.put("Z", 10);
+	   
+	   for(int i = 0; i < testStringA.length; i++) {
+		 for(Map.Entry<String, Integer> entry: letval.entrySet()) {
+			 if(entry.getKey().equals(testStringA[i])) {
+				 sum += entry.getValue();
+				 
+			 }
+		 }
+	   }
+	   	   
+		return sum;
 	}
 
 	/**
@@ -71,7 +133,40 @@ public class EvaluationService {
 	 */
 	public String cleanPhoneNumber(String string) {
 		// TODO Write an implementation for this method declaration
-		return null;
+		//	Steps to clean up phone numbers
+		//  		1. Remove "(",")", "-", "."
+		int slen = string.length();
+		
+		string = string.replaceAll("\\s", "");		
+		string = string.replaceAll("\\(","");
+		string = string.replaceAll("\\)","");
+		string = string.replaceAll("\\-", "");
+		string = string.replaceAll("\\.", "");
+		string = string.replaceAll("\\^+1", "");
+		string = string.trim();
+		//System.out.println(string);		
+		int sslen = string.length();
+		
+		if(sslen > 11) {
+			throw new IllegalArgumentException();
+				}
+
+		if(string.replaceAll("[0-9]", "").length() >= 1) {
+			throw new IllegalArgumentException();
+		}
+
+		//string = string.replaceAll("\\s", "");
+		
+	
+		//System.out.println(slen);
+		//System.out.println(string);
+		
+		//  		2. Remove "1 ", remove 1 and trailing space 
+		
+		
+		
+	
+		return string;
 	}
 
 	/**
@@ -84,7 +179,29 @@ public class EvaluationService {
 	 * @return
 	 */
 	public Map<String, Integer> wordCount(String string) {
-		// TODO Write an implementation for this method declaration
+
+		/*
+		 * 		1. convert string to array
+		 * 		2. use for loop to increment through array
+		 * 		3. us if statement to compare index
+		 * */
+		string.replaceAll(",", "\\");
+		String[] words = string.split("\\s");
+		int arrlen = words.length;
+		int[] sum = {0};
+		for(int x = 0; x < arrlen; x++) {
+			sum[x] = 0;
+		
+		//System.out.println(words[0]);
+		for(int i = 0; i < arrlen; i++) {
+			if(words[i] ==words[i+1]) {
+				sum[x] += 1;
+					
+				}
+				//System.out.println(sum);
+			}
+		}
+		
 		return null;
 	}
 
@@ -128,6 +245,8 @@ public class EvaluationService {
 
 		public int indexOf(T t) {
 			// TODO Write an implementation for this method declaration
+			
+			
 			return 0;
 		}
 
@@ -152,9 +271,9 @@ public class EvaluationService {
 	 * 
 	 * For example:
 	 * 
-	 * 9 is an Armstrong number, because 9 = 9^1 = 9 10 is not an Armstrong number,
+	 * 9 is an Armstrong number, because 9 = 9^1 = 9. 10 is not an Armstrong number,
 	 * because 10 != 1^2 + 0^2 = 2 153 is an Armstrong number, because: 153 = 1^3 +
-	 * 5^3 + 3^3 = 1 + 125 + 27 = 153 154 is not an Armstrong number, because: 154
+	 * 5^3 + 3^3 = 1 + 125 + 27 = 153, 154 is not an Armstrong number, because: 154
 	 * != 1^3 + 5^3 + 4^3 = 1 + 125 + 64 = 190 Write some code to determine whether
 	 * a number is an Armstrong number.
 	 * 
@@ -163,8 +282,55 @@ public class EvaluationService {
 	 */
 	public boolean isArmstrongNumber(int input) {
 		// TODO Write an implementation for this method declaration
+		
+//		Computer Armstrong Number
+//		1. Take input value and split digits into array
+	/*
+		
+		int power = Integer.toString(input).length();
+		int sum = 0;
+		//System.out.println(input);
+		int armnumber[] = new int[power];
+		int i = 0;
+		do {
+			armnumber[i] = input % 10;
+			input/= 10;
+			if(input % 10 == 0) {
+				//armnumber[i]=0;
+//					System.out.println(a);
+	//			}
+				
+				//System.out.println(input);
+			}
+			i++;
+		}
+		while(input != 0);
+//		2. call each value and raise to the power of the number length
+		
+		for( int j = 0; j < power; j++) {
+
+			sum += Math.pow(armnumber[j], power);
+						
+		}
+		System.out.println(sum);
+		if(sum == input) {
+			return true;
+		}
+		else {
+		
 		return false;
+		}
+		*/
+		//Using substrings
+		int sum = 0;
+		String incast = Integer.toString(input);
+		for( int i = 0; i < incast.length(); i++) {
+			sum += Math.pow(Integer.parseInt((incast.substring(i, i+1))), (double) incast.length()); 
+		}
+		return sum == input;
+		
 	}
+	
 
 	/**
 	 * 7. Compute the prime factors of a given natural number.
@@ -178,7 +344,30 @@ public class EvaluationService {
 	 */
 	public List<Long> calculatePrimeFactorsOf(long l) {
 		// TODO Write an implementation for this method declaration
-		return null;
+		List<Long> numl = new ArrayList<>();
+		for(int i = 2; i < l; i++) {
+			if(l%i == 0 && ifprime(i)) {
+				numl.add((long) i);
+			long l2 = l / i;
+			while(l2 % i == 0 && l2 > 0) {
+				l2 /=i;
+				numl.add((long) i);
+			}
+		}
+	}
+		if(l == 2) {
+			numl.add(2L);
+		}
+		//System.out.println(numl.toString());
+
+		return numl;
+	}
+	public boolean ifprime(long a) {
+		for(int i = 2; i < a; i++) {
+			if(a%i == 0) {
+				return false;
+			}
+		} return true;
 	}
 
 
@@ -216,8 +405,80 @@ public class EvaluationService {
 		 */
 		public static String encode(String string) {
 			// TODO Write an implementation for this method declaration
-			return null;
-		}
+			
+			int ilen = string.length();
+			String[] instr = string.toLowerCase().split("");
+			String[] oustr = new String[ilen];
+			   Map<String, String> cipherT = new HashMap<String, String>();
+			   int sum = 0;
+			   
+			   cipherT.put("a", "z");
+			   cipherT.put("b", "y");
+			   cipherT.put("c", "x");
+			   cipherT.put("d", "w");
+			   cipherT.put("e", "v");
+			   cipherT.put("f", "u");
+			   cipherT.put("g", "t");
+			   cipherT.put("h", "s");
+			   cipherT.put("i", "r");
+			   cipherT.put("j", "q");
+			   cipherT.put("k", "p");
+			   cipherT.put("l", "o");
+			   cipherT.put("m", "n");
+			   cipherT.put("n", "m");
+			   cipherT.put("o", "l");
+			   cipherT.put("p", "k");
+			   cipherT.put("q", "j");
+			   cipherT.put("r", "i");
+			   cipherT.put("s", "h");
+			   cipherT.put("t", "g");
+			   cipherT.put("u", "f");
+			   cipherT.put("v", "e");
+			   cipherT.put("w", "d");
+			   cipherT.put("x", "c");
+			   cipherT.put("y", "b");
+			   cipherT.put("z", "a");
+			   cipherT.put("1", "1");
+			   cipherT.put("2", "2");
+			   cipherT.put("3", "3");
+			   cipherT.put("4", "4");
+			   cipherT.put("5", "5");
+			   cipherT.put("6", "6");
+			   cipherT.put("7", "7");
+			   cipherT.put("8", "8");
+			   cipherT.put("9", "9");
+			   cipherT.put("0", "0");
+
+			   for( int i = 0; i < ilen; i++) {
+				   for(Map.Entry<String, String> entry: cipherT.entrySet()) { 
+			   		   if(entry.getKey().equals(instr[i])) {  
+			   			   oustr[i] = entry.getValue();
+			   
+			//System.out.println(instr[0]);
+			   }
+			   		  // if()
+			   		  // if(oustr[i].equals(null)) {
+			   			//   oustr[i].replaceAll(null, " ");
+			   		   //}
+			   		   
+		   }
+				    
+				   }
+		//System.out.println(Arrays.deepToString(oustr));	
+		
+		String enw = "";
+        StringBuilder stringB = new StringBuilder();
+
+        for (int j = 0; j < oustr.length; j++) {
+
+              string = string+ oustr[j];
+        } 
+        //enw.replaceAll("null", "\\s");
+            System.out.println(stringB);
+        
+        return stringB.toString();
+			//return Arrays.toString(oustr);
+	}
 
 		/**
 		 * Question 9
@@ -260,7 +521,36 @@ public class EvaluationService {
 	 */
 	public int solveWordProblem(String string) {
 		// TODO Write an implementation for this method declaration
-		return 0;
-	}
+		string = string.replace("What", "");
+		string = string.replace("is", "");
+		string = string.replace("by", "");
+		string = string.replace("?", "");
+				string = string.replaceAll("\\s", "");
 
+		string = string.replace("plus", " + ");
+		string = string.replace("minus", " - " );
+		string = string.replace("multiplied", " * ");
+		string = string.replace("divided", " / ");
+		
+		String[] wordp = string.split(" ");
+		//System.out.println(Arrays.toString(wordp));
+		switch(wordp[1]) {
+		case "+": 
+			return Integer.parseInt(wordp[0]) + Integer.parseInt(wordp[2]);
+		case "-": 
+			return Integer.parseInt(wordp[0]) - Integer.parseInt(wordp[2]);
+		case "*": 
+			return Integer.parseInt(wordp[0]) * Integer.parseInt(wordp[2]);
+		case "/": 
+			return Integer.parseInt(wordp[0]) / Integer.parseInt(wordp[2]);
+		
+		default:
+			throw new IllegalArgumentException();
+		}
+		
+		
+		
+		//System.out.println(string);
+			
+	}
 }
